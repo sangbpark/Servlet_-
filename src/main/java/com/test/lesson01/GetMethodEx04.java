767ed2ed11c31bac94fb253a40fefbee3d9080df
sangbpark <qkrtkdqo94@gmail.com>
2024-09-10 19:29:42 +0900

    stash 배움

--- src/main/java/com/test/lesson01/GetMethodEx04.java
+++ src/main/java/com/test/lesson01/GetMethodEx04.java
@@ -14,6 +14,7 @@ public class GetMethodEx04 extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// 응답 헤더 세팅
+		// 테스트 주석 추가
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html");
 		
